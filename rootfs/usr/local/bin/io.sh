8b43e6ad4f010e6a0d34a896b1fcb0b986003e77
ElevenNotes <github@11notes.ch>
2024-10-14 11:54:17 +0200

    sync file deletion directly

--- rootfs/usr/local/bin/io.sh
+++ rootfs/usr/local/bin/io.sh
@@ -3,14 +3,24 @@
 
   inotifyd_queue(){
     find ${IODIR} -type f -not -newermt "-${RSYNC_TRANSFER_DELAY} seconds" -exec rm -f {} \;
-    INOTIFYD_PATH=$(echo "${1}" | sed -e "s@\/@_@g")
-    INOTIFYD_FILE="${IODIR}/${INOTIFYD_PATH}"
-    if [ -f "${INOTIFYD_FILE}" ]; then
-      elevenLogJSON debug "queue busy for ${1}"
-      return 1
-    fi
-    elevenLogJSON debug "queue created for ${1}"
-    touch ${INOTIFYD_FILE}
+
+    case "${1}" in
+      d)
+        return 0
+      ;;
+
+      *)
+        INOTIFYD_PATH=$(echo "${2}" | sed -e "s@\/@_@g")
+        INOTIFYD_FILE="${IODIR}/${INOTIFYD_PATH}"
+        if [ -f "${INOTIFYD_FILE}" ]; then
+          elevenLogJSON debug "queue busy for ${2}"
+          return 1
+        fi
+        elevenLogJSON debug "queue created for ${2}"
+        touch ${INOTIFYD_FILE}
+      ;;
+    esac
+    
     return 0
   }  
 
@@ -24,7 +34,7 @@
         elevenLogJSON debug "starting to watch directory ${2}/${3}:${MASK}"
         /sbin/inotifyd /usr/local/bin/io.sh ${2}/${3}:${MASK} &
 
-        if inotifyd_queue "${2}/${3}"; then
+        if inotifyd_queue "${1}" "${2}/${3}"; then
           if [ ${RSYNC_TRANSFER_DELAY} -gt 0 ]; then nq -q sleep ${RSYNC_TRANSFER_DELAY}; fi
           for HOST in ${SSH_HOSTS}; do
             SSH_HOST=$(echo "${HOST}" | awk '{split($0,a,":"); print a[1]}')
@@ -34,7 +44,7 @@
           done
         fi
       else
-        if inotifyd_queue "${2}"; then
+        if inotifyd_queue "${1}" "${2}"; then
           if [ ${RSYNC_TRANSFER_DELAY} -gt 0 ]; then nq -q sleep ${RSYNC_TRANSFER_DELAY}; fi
           for HOST in ${SSH_HOSTS}; do
             SSH_HOST=$(echo "${HOST}" | awk '{split($0,a,":"); print a[1]}')
